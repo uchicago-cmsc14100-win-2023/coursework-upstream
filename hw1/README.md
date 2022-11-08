09ba1ed99b1894356a07720dd6ccdfa2f3803e85
Anne Rogers <amr@cs.uchicago.edu>
2022-11-08 16:26:25 -0600

    Added materials for HW #1

---
+++ hw1/README.md
@@ -0,0 +1,7 @@
+CMSC 14100, Winter 2023
+Homework #1
+
+hw1.py: You will modify hw1.py to do your work for HW #1.
+
+README.md: the file you are reading.
+
---
+++ hw1/hw1.py
@@ -0,0 +1,24 @@
+"""
+CMSC 14100, Winter 2023
+Homework #1
+
+We will be using anonymous grading.  Please do not add your name to the file.
+
+People Consulted:
+   For future assignments, you will be expected to list anyone (other than the
+   course staff that you consulted about this assignment).
+
+Online resources consulted:
+   For future assignments, you will be expected to list the URL of
+   any online resources that you use to complete this assignment.
+"""
+
+def add2():
+    ### Replace None with 2 + 2 in the next line.
+    result = None
+
+    ### Do not modify the next line.
+    return result
+
+if __name__ == "__main__":
+    print("2 + 2 is", add2())
